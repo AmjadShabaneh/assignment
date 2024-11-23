7516015e3fb39da4b82086d065593941a1242edd
Amjad Shabaneh <213510@ppu.edu.ps>
2024-11-23 14:01:43 +0200

    React assignment

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,16 @@
+
+import React,{useState} from 'react'
+import Button from './components/Button';
+import Alert from './components/Alert';
+const App = () => {
+  const [alertVisible , SetAlertVisibility] =  useState(false);
+  return (
+    <div>
+      
+      { alertVisible && <Alert onClose={()=>SetAlertVisibility(false)}>My alert</Alert>}
+      <Button  children='Click me' onClick={()=>SetAlertVisibility(true)}></Button>
+    </div>
+  )
+}
+
+export default App
